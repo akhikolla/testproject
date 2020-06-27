1ce1b1bacf1113f81e657987c1186a4e13612d42
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-27 15:50:23 -0700

    makevars and no script

--- R/deepstate_ci_setup.R
+++ R/deepstate_ci_setup.R
@@ -7,12 +7,16 @@ deepstate_ci_setup<-function(path){
     print(".travis.yml doesn't exist in your project creating a new file !!!")
     file.create(travis_path,recursive=TRUE)
     write_to_travis <- ""
-    write_to_travis <-paste0(write_to_travis,"warnings_are_errors: false\nlanguage: r\nsudo: required\n")
-    write_to_travis<-paste0(write_to_travis,"r_packages:\n\t- RInside\n\t- devtools\nafter_success:\n\t")
-    write_to_travis<-paste0(write_to_travis,"- Rscript -e 'devtools::install();devtools::test()'\nr_check_args: '--as-cran --use-valgrind'")
-    write_to_travis<-paste0(write_to_travis,"\naddons:\napt:\n packages:\n\t- valgrind\nenv:\n\t-VALGRIND_OPTS='--leak-check=full --track-origins=yes'")
+    write_to_travis <-paste0(write_to_travis,"warnings_are_errors: false\nlanguage: r\nsudo: required\n\n")
+    write_to_travis<-paste0(write_to_travis,"r_packages:\n\t- RInside\n\t- devtools\n\nafter_success:\n\t")
+    write_to_travis<-paste0(write_to_travis,"- Rscript -e 'devtools::install();devtools::test()'\n\nr_check_args: '--as-cran --use-valgrind'\n")
+    write_to_travis<-paste0(write_to_travis,"\naddons:\n apt:\n packages:\n\t- valgrind\n\nenv:\n\t-VALGRIND_OPTS='--leak-check=full --track-origins=yes'\n\n")
     write_to_travis<-paste0(write_to_travis,"script:\n\t- |\n\ttravis_wait 60 R CMD build .\n\ttravis_wait 60 R CMD check testproject*tar.gz")
     
     write(write_to_travis,travis_path,append=TRUE)
   }
+  travis.checklist <- nc::capture_all_str(travis_path,
+                                          "r_packages:",
+                                          r_packages="(?:\n\t.*)*")
+  
 }
