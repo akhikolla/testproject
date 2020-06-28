e1bcafc6bc0d914e8cf092d2b29dd0a1217ba172
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-27 18:55:25 -0700

    with removing src files

--- R/deepstate_ci_setup.R
+++ R/deepstate_ci_setup.R
@@ -9,25 +9,46 @@ deepstate_ci_setup<-function(path){
     file.create(travis_path,recursive=TRUE)
     write_to_travis <- ""
     write_to_travis <-paste0(write_to_travis,"warnings_are_errors: false\nlanguage: r\nsudo: required\n\n")
-    write_to_travis<-paste0(write_to_travis,"r_packages:\n\t- RInside\n\t- devtools\n\nafter_success:\n\t")
+    write_to_travis<-paste0(write_to_travis,"r_packages:\n  - RInside\n  - devtools\n\nafter_success:\n  ")
     write_to_travis<-paste0(write_to_travis,"- Rscript -e 'devtools::install();devtools::test()'\n\nr_check_args: '--as-cran --use-valgrind'\n")
-    write_to_travis<-paste0(write_to_travis,"\naddons:\n\tapt:\n\t\tpackages:\n\t\t\t- valgrind\n\nenv:\n\t-VALGRIND_OPTS='--leak-check=full --track-origins=yes'\n\n")
-    write_to_travis<-paste0(write_to_travis,"script:\n\t- |\n\ttravis_wait 60 R CMD build .\n\ttravis_wait 60 R CMD check testproject*tar.gz")
+    #write_to_travis<-paste0(write_to_travis,"\naddons:\n  apt:\n\t\tpackages:\n\t\t\t- valgrind\n\nenv:\n\t-VALGRIND_OPTS='--leak-check=full --track-origins=yes'\n\n")
+    write_to_travis<-paste0(write_to_travis,"\napt_packages:\n  - valgrind\n\n")
+    write_to_travis<-paste0(write_to_travis,"env:\n  - VALGRIND_OPTS='--leak-check=full --track-origins=yes'")
+    write_to_travis<-paste0(write_to_travis,"script:\n  - |\n  - travis_wait 60 R CMD build .\n  - travis_wait 60 R CMD check testproject*tar.gz")
     
     write(write_to_travis,travis_path,append=TRUE)
   }
   travis_lines <- readLines(travis_path)
   r_packages <- nc::capture_all_str(travis_path,
                                           "r_packages:",
-                                          list="(?:\n\t.*)*")
+                                          list="(?:\n  .*)*")
   apt_packages <- nc::capture_all_str(travis_path,
                                     "apt_packages:",
-                                list="(?:\n\t.*)*")
-  
+                                list="(?:\n  .*)*")
+  r_check_args <- nc::capture_all_str(travis_path,"r_check_args:",list=".*")
+  env <- nc::capture_all_str(travis_path,"env:",list="(?:\n  .*)*")
   if(grepl("RInside",r_packages$list))
     print("RInside present")
   else{
-    travis_lines <- gsub("r_packages:","r_packages:\n\t- RInside",travis_lines)
-    cat(travis_lines, file=travis_path, sep="\n")
+    travis_lines <- gsub("r_packages:","r_packages:\n  - RInside",travis_lines)
   }
+  if(grepl("valgrind",apt_packages$list))
+    print("valgrind present")
+  else{
+    travis_lines <- gsub("apt_packages:","apt_packages:\n  - valgrind",travis_lines)
+  }
+  if(grepl("'--as-cran --use-valgrind'",r_check_args$list)){
+    print("arg valgrind present")
+  }
+  else{
+    travis_lines <- gsub("r_check_args:","r_check_args: '--as-cran --use-valgrind'",travis_lines)
+  }
+  if(grepl("'VALGRIND_OPTS='--leak-check=full --track-origins=yes'",env$list)){
+    print("environment for valgrind is set")
+  }
+  else{
+    travis_lines <- gsub("env:","env: 'VALGRIND_OPTS='--leak-check=full --track-origins=yes'",travis_lines)
+  }
+  
+  cat(travis_lines, file=travis_path, sep="\n")
   }
